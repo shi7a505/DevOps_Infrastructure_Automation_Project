273adef703e6fda91f09decc999765e97a13e7ec
Aiemond‬‏ <m.shiha1111@gmail.com>
2025-10-29 00:55:40 +0200

    Update variables.tf

--- terraform-bootstrap/variables.tf
+++ terraform-bootstrap/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS Region"
   type        = string
-  default     = "us-north-1"
+  default     = "eu-north-1"
 }
 
 variable "instance_type" {
@@ -13,11 +13,11 @@ variable "instance_type" {
 variable "terraform-key" {
   description = "AWS Key Pair Name"
   type        = string
-  # هتحط اسم الـ Key Pair بتاعك هنا
+ 
 }
 
 variable "197.43.146.158" {
   description = "Your IP for SSH access"
   type        = string
-  # هتحط IP جهازك هنا
+ 
 }
